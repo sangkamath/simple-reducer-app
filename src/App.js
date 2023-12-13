72c9fb2c83201a03349919d161bad48b5e4f7aee
sangkamath <twinkle_kamath@yahoo.com>
2023-12-13 20:08:00 +0530

    Initial reducer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { useReducer} from 'react';
+
+const initialTodos = [
+  {
+    id: 1,
+    title: "Todo 1",
+    complete: false
+  }, 
+  {
+    id: 2,
+    title: "Todo 2",
+    coomplete: false
+  }
+];
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case "COMPLETE": 
+      return state.map((todo) => {
+        if(todo.id === action.id) {
+          return {...todo, complete: !todo.complete}
+        } else {
+          return todo;
+        }
+      })
+    default:
+      return state;
+  }
+}
 
 function App() {
+  const [ todos, dispatch ] = useReducer(reducer, initialTodos);
+
+  const handleComplete = (todo) => {
+    dispatch({ type: "COMPLETE", id: todo.id});
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          <label>
+            <input 
+              type="checkbox"
+              checked={todo.complete}
+              onChange={() => handleComplete(todo)} 
+            />
+            {todo.title}
+          </label>
+        </div>
+      ))}
     </div>
   );
 }
